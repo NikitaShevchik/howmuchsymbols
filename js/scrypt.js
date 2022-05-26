f494b19ee913d4646fa011cfbd8f11052275461f
NikitaShevchik <shevchik.nikita@gmail.com>
2022-05-26 14:50:36 +0300

    procentage+counter

--- js/scrypt.js
+++ js/scrypt.js
@@ -3,22 +3,38 @@
 const textUnderTextarea = document.querySelector(".counter__text");
 const textarea = document.querySelector(".counter__textarea");
 
-
 textarea.addEventListener("blur", countWords);
 textarea.addEventListener("focus", emptyText);
 
 var counterWord;
+var counterSymbol;
 function countWords() {
     var textValue = textarea.value.length;
     counterWord = 1;
+    counterSymbol = 0;
     for (let i = 0; i < textValue - 1; i++) {
         if (textarea.value[i] === " ") {
             counterWord += 1;
         }
     }
-    textUnderTextarea.innerHTML = `<p>В тексте ${counterWord} слов</p><p>В тексте ${textarea.value.length} символов</p>`;
+    for (let i = 0; i < textValue; i++) {
+        if (textarea.value[i] !== " ") {
+            counterSymbol++;
+        }
+    }
+    var procentSymbol = ((counterSymbol / textValue) * 100).toFixed(2);
+    textUnderTextarea.innerHTML = `<p>В тексте ${counterWord} слов</p><p>В тексте ${textValue} символов</p>
+    <p>В тексте ${counterSymbol} символов без пробелов</p>
+    <p>В тексте ${procentSymbol} % символов</p>
+    <p>В тексте ${100 - procentSymbol} % пробелов</p>`;
 }
-
 function emptyText() {
     textUnderTextarea.innerHTML = '';
 }
+textarea.addEventListener("keydown", function (key) {
+    if (key.code == 'Space' && textarea.value[textarea.value.length - 1] == ' ') {
+        key.preventDefault();
+    } else if (key.code == 'Enter') {
+        key.preventDefault();
+    }
+});
